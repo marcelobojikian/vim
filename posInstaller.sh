9104543c2f6e651615f4f389a14c7377641e8ff3
marcelobojikian <marcelo.bojikian@gmail.com>
2023-08-25 23:46:09 +0100

    Create posInstaller.sh

---
+++ posInstaller.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if ! command -v vim &>/dev/null; then
+  echo "Vim not installed yet"
+  exit 1
+fi
+
+DIST_VIM=~/.vim
+mkdir -p $DIST_VIM/autoload $DIST_VIM/bundle $DIST_VIM/colors
+
+if [ -f "$DIST_VIM/autoload/pathogen.vim" ]; then
+  echo "[X] Pathogen"
+else
+  echo "Installing the Pathogen packge manager"
+  curl -LSso $DIST_VIM/autoload/pathogen.vim "https://tpo.pe/pathogen.vim"
+  echo "[X] Pathogen"
+fi
+
+if [ -d "$DIST_VIM/bundle/nerdtree" ]; then
+  echo "[X] NERDtree"
+else
+  echo "Installing the NERDtree file system explorer"
+  git clone https://github.com/preservim/nerdtree.git $DIST_VIM/bundle/nerdtree
+  echo "[X] NERDtree"
+fi
+
+if [ -f "$DIST_VIM/colors/Tomorrow-Night-Eighties.vim" ]; then
+  echo "[X] vim-tomorrow-theme"
+else
+	echo "Installing theme vim-tomorrow-theme"
+	wget https://raw.githubusercontent.com/chriskempson/vim-tomorrow-theme/master/colors/Tomorrow-Night-Eighties.vim -P $DIST_VIM/colors/
+	echo "[X] vim-tomorrow-theme"
+fi
+
+cat << EOF > ~/.vimrc
+execute pathogen#infect()
+colorscheme Tomorrow-Night-Eighties
+set tabstop=2
+filetype plugin indent on
+syntax on
+set shiftwidth=2
+set backspace=2
+set number
+
+set relativenumber
+set incsearch
+set hlsearch
+EOF
